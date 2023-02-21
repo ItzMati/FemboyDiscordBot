78c0985c0f05687c3c9245e430559ff39cdf2981
ItzMati <matip90@op.pl>
2023-02-21 18:12:33 +0000

    Adding the files
    yeah

---
+++ femboy guessing.py
@@ -0,0 +1,62 @@
+import os
+import discord
+from discord import app_commands
+from discord.ext import commands
+import random as randomn
+import imagegetter
+
+TOKEN = "MTA3NzMyNTY3OTExODUyNDUwOA.GV-A9B.0WbVrVdPg4Gg4URxmOhvM10JvqFqQ8ET9Sn8kw"
+
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    try:
+        synced = await bot.tree.sync()
+        print(f"synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+
+
+@bot.tree.command(name="send_number", description="Sends back the number you give")
+@app_commands.describe(number="The number that the bot will repeat back to you")
+async def send_number(ctx, number : int):
+    response = "the number you said was "+ str(number)
+    await ctx.response.send_message(response)
+
+a=""
+def gen_numbers(ranger, amoun):
+    global a
+    a=""
+    for i in range(amoun):
+        a +="\n"+str(randomn.randint(0,int(ranger)))
+
+@bot.tree.command(name = "random", description="Sends a defined amount of random numbers")
+@app_commands.describe(high="The highest number you can generate", amount="The amount of numbers that will be generated")
+async def random(ctx, high : int, amount : int):
+    gen_numbers(high, amount)
+    response = a
+    await ctx.response.send_message(response)
+    
+
+@bot.tree.command(name="send_image", description="Sends an image")
+async def send_image(ctx):
+    await ctx.response.send_message(file=discord.File('meme.png'))
+
+@bot.tree.command(name="send_femboy", description="Sends a random picture of a femboy")
+@app_commands.describe(place="The type of feed you want your image from (new, hot, top or rising)")
+async def send_femboy(ctx, place : str):
+    try:
+        imagegetter.function(feed=place)
+        await ctx.response.send_message(file=discord.File('image_name0.jpg'))
+    except:
+        response="Something went wrong. The type of feed you inputed was probably incorrect. Check your spelling"
+        await ctx.response.send_message(response)
+    
+
+bot.run(TOKEN)
---
+++ imagegetter.py
@@ -0,0 +1,58 @@
+import requests
+import json
+from pprint import pprint
+import sys
+import requests
+import random
+
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+
+sub='femboy'
+
+waba = 0
+imagelist= []
+
+
+client_id = 'uHaDpx5N5mQ-IpXQ3W-fkw'
+secret_key = 'x8AyniGBIGSPXKUsIVxnmFRdnWZWlg'
+
+auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
+    
+data = {
+    'grant_type': 'password',
+    'username': 'glubstergluber',
+    'password': 'haslomati1234'
+    }
+
+headers = {'User-Agent': 'MyAPI/0.0.1'}
+
+
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+res.json()
+
+token=res.json()['access_token']
+
+headers['Authorization'] = f'bearer {token}'
+
+def function(feed):
+    global waba
+    link= 'https://oauth.reddit.com/r/'+sub+'/'+feed
+
+    bruh = requests.get(link, headers=headers)
+
+    for post in bruh.json()['data']['children']:
+        idk = (str(post['data']['url']).translate(non_bmp_map))
+        if 'comments' in idk:
+            waba +=1
+        else:
+            imagelist.append(idk)
+
+
+    for i in range(1):
+        item = random.choice(imagelist)
+        img_data = requests.get(item).content
+        with open('image_name'+str(i)+'.jpg', 'wb') as handler:
+            handler.write(img_data)
+
+#function(feed="top")
